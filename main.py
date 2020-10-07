21f56e605fa5faac4206f6070a6a23f11b85fad0
VanshWadhwa <72441052+VanshWadhwa@users.noreply.github.com>
2020-10-07 19:26:43 +0530

    Changed absolute paths to relative path
    Changed absolute paths of image , font to relative path 
    Added more comments for easy readibility

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
+#Importing the modules
+#Piil -> Pillow module for getting the image and adding font and saving it
 
 
-def generate_meme(image_path, top_text, bottom_text='', font_path='C:\\Users\\Shaily Priya\\Desktop\\New folder\\font\\impact.ttf', font_size=9):
+def generate_meme(image_path, top_text, bottom_text='', font_path='\\font\\impact.ttf', font_size=9):
+	""""Loading image , adding font , wraping text and saving the image """
+
 	# load image
-	im = Image.open("img.jpg")
-	draw = ImageDraw.Draw(im)
-	image_width, image_height = im.size
+	im = Image.open(image_path)#getting the image object
+	
+	draw = ImageDraw.Draw(im)#Drawing Object
+	image_width, image_height = im.size #im.size returns the tuple(width , height)
 	
 	# load font
-	font = ImageFont.truetype(font="C:\\Users\\Shaily Priya\\Desktop\\New folder\\font\\impact.ttf", size=int(image_height*font_size)//100)
-
+	font = ImageFont.truetype(font=font_path, size=int(image_height*font_size)//100)#getting the font as .ttf
+	
 	# convert text to uppercase
 	top_text = top_text.upper()
 	bottom_text = bottom_text.upper()
@@ -39,9 +44,10 @@ def generate_meme(image_path, top_text, bottom_text='', font_path='C:\\Users\\Sh
 
 	# save meme
 	im.save('meme-' + im.filename.split('/')[-1])
+	print("Imaged Saved Successfully")
 
 
 if __name__ == '__main__':
 	top_text = "I dont always make memes"
 	bottom_text = "But when I do, I use Python"
-	generate_meme('./idontalways.jpg', top_text=top_text, bottom_text=bottom_text)
+	generate_meme('img.jpg', top_text=top_text, bottom_text=bottom_text)
